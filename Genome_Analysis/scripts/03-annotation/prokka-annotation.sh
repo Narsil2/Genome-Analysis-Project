f238b69fe751900fd116bba438f19eb176fbbd57
Yogesh Aravamudhan <yogesh22@pelle2.uppmax.uu.se>
2026-04-24 15:39:07 +0200

    Prokka annotation

---
+++ Genome_Analysis/scripts/03-annotation/prokka-annotation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 1
+#SBATCH -t 00:15:00
+#SBATCH -J job_name
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
+module load prokka
+
+prokka --outdir ~/Genome_Analysis/outputs/03-annotation/ --prefix E.faecium-annotation \
+	~/Genome_Analysis/outputs/01-genome-assembly/E.faecium.contigs.fasta
