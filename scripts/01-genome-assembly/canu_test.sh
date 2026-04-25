70ac163925017feea3e4f68c5aa0f96b67a24042
Yogesh Aravamudhan <yogesh22@pelle1.uppmax.uu.se>
2026-04-25 15:39:24 +0200

    cleaning up

--- scripts/01-genome-assembly/canu_test.sh
+++
@@ -1,7 +0,0 @@
-
-canu \
-        -p E.faecium \
-        genomeSize=4.8m \
-        useGrid=false \
--pacbio-raw /home/yogesh22/Genome_Anlaysis/raw_data/genomics_data/PacBio/*.fastq.gz 1>canu_assembly_test.stdout 2>canu_assembly_test.stderr
-
