8d6c9bcda6221f3c7e6db0078cfd2ca65898bae0
Yogesh Aravamudhan <yogesh22@pelle1.uppmax.uu.se>
2026-04-28 16:26:13 +0200

    synteny analysis

---
+++ Genome_Analysis/scripts/04-homology/blast_search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 00:15:00
+#SBATCH -J blastn_search
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
+module load BLAST+
+
+blastn -db 'E.faecium-strainVRE095.fasta' \
+	-query /home/yogesh22/Genome_Analysis/outputs/01-genome-assembly/E.faecium.contigs.fasta \
+	-out /home/yogesh22/Genome_Analysis/outputs/04-homology/blast.hits
+
