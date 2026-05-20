fdf44ca36d49bb874a800838c57536307ae9550a
Yogesh Aravamudhan <yogesh22@pelle2.uppmax.uu.se>
2026-05-20 16:37:05 +0200

    bwa alignment updated

--- Genome_Analysis/scripts/06-alignment/bwa_mem.sh
+++ Genome_Analysis/scripts/06-alignment/bwa_mem.sh
@@ -7,18 +7,44 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=%x.%j.out
 
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 4
+#SBATCH -t 04:00:00
+#SBATCH -J bwa_mem
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
 module load BWA
+module load SAMtools
 
-bwa index -p Efaecium ~/Genome_Analysis/reference-genome/*.fna
+# Reference (index only once externally ideally)
+REF=~/Genome_Analysis/reference-genome/*.fna
+PREFIX=Efaecium
 
-for file in ~/Genome_Analysis/raw_data/transcriptomics_data/RNA-Seq_BH/trimmed/*.fastq.gz
+# If not already indexed, run once manually:
+# bwa index -p $PREFIX $REF
+
+# --- BH samples ---
+for r1 in ~/Genome_Analysis/raw_data/transcriptomics_data/RNA-Seq_BH/trimmed/*_paired_*_pass_1.fastq.gz
 do
-    bwa mem Efaecium "$file" \
-    > ~/Genome_Analysis/outputs/06-alignment/BH/$(basename "${file%.fastq.gz}").sam
+    r2=${r1/_pass_1/_pass_2}
+    base=$(basename "${r1%%_pass_1.fastq.gz}")
+
+    bwa mem -t 4 $PREFIX "$r1" "$r2" | \
+    samtools view -bS | \
+    samtools sort -o ~/Genome_Analysis/outputs/06-alignment/BH/${base}.sorted.bam
 done
 
-for file in ~/Genome_Analysis/raw_data/transcriptomics_data/RNA-Seq_Serum/trimmed/*.fastq.gz
+
+# --- Serum samples ---
+for r1 in ~/Genome_Analysis/raw_data/transcriptomics_data/RNA-Seq_Serum/trimmed/*_paired_*_pass_1.fastq.gz
 do
-    bwa mem Efaecium "$file" \
-    > ~/Genome_Analysis/outputs/06-alignment/Serum/$(basename "${file%.fastq.gz}").sam
+    r2=${r1/_pass_1/_pass_2}
+    base=$(basename "${r1%%_pass_1.fastq.gz}")
+
+    bwa mem -t 4 $PREFIX "$r1" "$r2" | \
+    samtools view -bS | \
+    samtools sort -o ~/Genome_Analysis/outputs/06-alignment/Serum/${base}.sorted.bam
 done
